66f43bf84f7b11b838f068ff841b9955dfaad09f
Jens A. Koch <jakoch@web.de>
2025-07-23 17:39:47 +0200

    Create vscode/extensions.sh

---
+++ vscode/extensions.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Create the list of VSCode extensions and prefix them with "code --install-extension" for easy (re)installation.
+# code --list-extensions | xargs -L 1 echo code --install-extension
